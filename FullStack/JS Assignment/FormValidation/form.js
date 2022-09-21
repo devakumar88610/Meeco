9e2510e85f5fcb8dabb5c1f0f101d012143819b8
devakumar88610 <devakumar88610@gmail.com>
2022-09-21 19:52:08 +0530

    okie

--- FullStack/JS Assignment/FormValidation/form.js
+++ FullStack/JS Assignment/FormValidation/form.js
@@ -8,3 +8,17 @@
 // 6. min
 // 7. min & max
 // 8. tab
+
+let firstName = document.getElementById('fname-error')
+let lastName = document.getElementById('lname-error')
+let mobileNumber = document.getElementById('mobile-error')
+let password1 = document.getElementById('password-error1')
+let password2 = document.getElementById('password-error2')
+
+function validateName1(){
+    let fname = document.getElementById('first-name').value;
+    if(fname == ''){
+        document.getElementById('fname-error').innerHTML = 'Enter valid name'
+    }
+    if(isNan(fname) || fname < 1 || fname > 10)
+}
